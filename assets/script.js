0844ba6605aea307a3c0d8d6a19e24fce8db1d97
Rick <95931094+confusedicarus@users.noreply.github.com>
2022-02-22 10:00:19 -0700

    forgot to save last night

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,19 @@
 searchBtn = document.getElementById("searchBtn");
-var weatherHour = "api.openweathermap.org/data/2.5/weather?";
-var weatherKey = "appid=70caa068c4969182efecd3dc706a38d9";
-console.log()
 
 searchBtn.addEventListener("click", function () {
   navigator.geolocation.getCurrentPosition(function (position) {
+    var weatherHour = "https://api.openweathermap.org/data/2.5/weather?";
+    var weatherKey = "&appid=70caa068c4969182efecd3dc706a38d9";
     var userLat = position.coords.latitude;
     var userLong = position.coords.longitude;
     console.log(userLat);
     console.log(userLong);
-    var latShort = userLat.toFixed(2);
-    var lonShort = userLong.toFixed(2);
-        fetch(
-        weatherHour + "lat=" + latShort + "&lon=" + lonShort + weatherKey
-        ).then(function (response) {
+    var requestUrl =
+      weatherHour + "lat=" + userLat + "&lon=" + userLong + weatherKey;
+    console.log(requestUrl);
+    fetch(requestUrl)
+      .then(function (response) {
         return response.jason();
-    });
+      });
+      });
   });
-});
