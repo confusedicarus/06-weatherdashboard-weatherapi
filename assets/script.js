29664f7b2b88086d049f2e53129f991d25067e87
Rick <95931094+confusedicarus@users.noreply.github.com>
2022-02-22 19:24:11 -0700

    lots of work to be done still. next step is creating the function for getting the city search event.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,18 @@
 searchBtn = document.getElementById("searchBtn");
+var weatherCall = "https://api.openweathermap.org/data/2.5/weather?";
+var weatherKey = "&appid=70caa068c4969182efecd3dc706a38d9";
+var lastCity = "";
+var currentCity = "";
+
+function getCity(event) {
+  var city = document.querySelector("input");
+  
+
+}
+
 
 searchBtn.addEventListener("click", function () {
   navigator.geolocation.getCurrentPosition(function (position) {
-    var weatherHour = "https://api.openweathermap.org/data/2.5/weather?";
-    var weatherKey = "&appid=70caa068c4969182efecd3dc706a38d9";
     var userLat = position.coords.latitude;
     var userLong = position.coords.longitude;
     console.log(userLat);
@@ -11,9 +20,8 @@ searchBtn.addEventListener("click", function () {
     var requestUrl =
       weatherHour + "lat=" + userLat + "&lon=" + userLong + weatherKey;
     console.log(requestUrl);
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.jason();
-      });
-      });
+    fetch(requestUrl).then(function (response) {
+      return response.jason();
+    });
   });
+});
